c2c09cf6da2376eec10e43b20d8d8fd28f1098b5
unknown <nick_clymer2018@yahoo.com>
2021-03-15 18:32:02 -0600

    testing formats

--- js/script.js
+++ js/script.js
@@ -4,51 +4,60 @@ var rootEl = $('#root');
 // var titleEl = document.createElement("h1");
 var titleEl = $('<h1>');
 
-// titleEl.textContent = "Hello friends";
-titleEl.text('Hello friends');
+// var today = moment();
+// var weekDay = today.format("[Today is] dddd")
+// $('#').text(weekDay);
 
-// titleEl.className = 'fancy';
-titleEl.attr('class', 'fancy');
+// titleEl.textContent = "Hello friends";
+titleEl.text('Work Day Scheduler');
+
+// titleEl.className = 'header';
+titleEl.attr('class', 'header');
 
 // titleEl.classList.add('p-5') - (`p-5` is for padding)
 titleEl.addClass('p-5');
 
 // titleEl.style.border = "rgb(122, 242, 242) 3px solid";
-titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
+// titleEl.css('border-bottom', 'rgb(122, 242, 242) 5px solid');
 
 // rootEl.appendChild(titleEl);
 rootEl.append(titleEl);
 
 // titleEl.append("Welcome to jQuery");
-rootEl.append('<h2>With jQuery we can:</h2>');
+rootEl.append('<h2>Hourly plan:</h2>');
 
-var abilities = [
-  'Select',
-  'Create',
-  'Style',
-  'Animate',
-  'Traverse',
-  'Event Listen',
-  'much more',
-];
+// var hoursArray = [
+//   '06:00',
+//   '07:00',
+//   '08:00',
+//   '09:00',
+//   '10:00',
+//   '11:00',
+//   '12:00',
+//   '13:00',
+//   '14:00',
+//   '15:00',
+//   '16:00',
+//   '18:00',
+//   '19:00',
+//   '20:00',
+//   '21:00',
+// ];
 
-for (var i = 0; i < abilities.length; i++) {
-  // Create a new `<div>` for each ability and its text content
-  var abilityEl = $('<li>');
+// for (var i = 0; i < hoursArray.length; i++) {
+//   // Create a new `<div>` for each ability and its text content
+//   var hoursEl = $('<li>');
 
-  // abilityEl.textContent = abilities[i];
-  abilityEl.text(abilities[i]);
+//   // abilityEl.textContent = abilities[i];
+//   hoursEl.text(hoursArray[i]);
 
-  // `my-3` class adds margin on top and bottom
-  abilityEl.addClass('my-3');
+//   // `my-3` class adds margin on top and bottom
+//   hoursEl.addClass('my-3');
 
-  // Alternatively
-  // var abilityEl = $("<div>" + abilities[i] + "</div>");
+//   // Add this new `<div>` to the abilities `<div>` container element.
+//   rootEl.append(hoursEl);
+// }
 
-  // Add this new `<div>` to the abilities `<div>` container element.
-  rootEl.append(abilityEl);
-}
-
-// select all `<div>` elements on the page
-// document.querySelectorAll('div');
-console.log($('div'));
+// // select all `<div>` elements on the page
+// // document.querySelectorAll('div');
+// console.log($('div'));
